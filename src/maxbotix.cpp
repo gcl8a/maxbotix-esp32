4ffabd37f725323aa1ad829da959a15b26f9258e
Greg <lewin.gc@gmail.com>
2021-10-23 17:45:52 -0400

    comments

--- src/maxbotix.cpp
+++ src/maxbotix.cpp
@@ -92,11 +92,12 @@ uint16_t MaxBotix::checkEcho(void)
 
 uint16_t MaxBotix::readMCP3002(void)
 {
-  //this will command the MCP to take a reading on ADC1; the datasheet has details
+  // This will command the MCP to take a reading on CH0
+  // Figure 6.1 of the datasheet shows the bit arrangement
   uint16_t cmdByte = 0x6800; 
 
   //start the SPI session
-  SPISettings spiSettings; //defaults to (1000000, MSBFIRST, SPI_MODE0), which is what we want
+  SPISettings spiSettings; //defaults to (clk freq = 1000000, MSBFIRST, SPI_MODE0), which is what we want
   SPI.beginTransaction(spiSettings); 
   
   //open communication with the MCP3002
@@ -104,7 +105,7 @@ uint16_t MaxBotix::readMCP3002(void)
 
   //this line both sends the command to read AND retrieves the result
   //the leading bits are indeterminate and need to be stripped off
-  uint16_t ADCvalue = SPI.transfer16(cmdByte) & 0x02ff;
+  uint16_t ADCvalue = SPI.transfer16(cmdByte) & 0x03ff;
   
   //end communication
   digitalWrite(SS, HIGH); 
